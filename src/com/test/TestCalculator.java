e92bfcdc8809b28beecc3195bdfbb36697c21654
vbontha <vinodkumar.bontha@progress.com>
2019-04-24 17:57:07 +0530

    Initial Committ

---
+++ src/com/test/TestCalculator.java
@@ -0,0 +1,28 @@
+package com.test;
+
+import org.junit.Test;
+
+import com.progress.Calculator;
+
+import junit.framework.Assert;
+
+public class TestCalculator {
+	
+	private static Calculator calculator = new Calculator();
+	
+	@Test
+	public void testAddition() {
+		Assert.assertEquals(13,calculator.addition(10, 3));
+	}
+	
+	@Test
+	public void testSubstraction() {
+		Assert.assertEquals(2, calculator.substraction(5, 3));
+	}
+	
+	@Test
+	public void testMultiplication() {
+		Assert.assertEquals(15, calculator.multiplication(5, 3));
+	}
+
+}
