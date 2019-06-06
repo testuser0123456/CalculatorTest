e959a5a1ae6489eb8abcf58dae1ca581a517de65
vbontha <vinodkumar.bontha@progress.com>
2019-06-06 17:28:54 +0530

    added teamcity messages

--- src/com/test/UnrecognizedCases.java
+++ src/com/test/UnrecognizedCases.java
@@ -9,15 +9,21 @@ public class UnrecognizedCases {
 	private static Calculator calculator = new Calculator();
 	
 	public void testAddition() {
+		System.out.println("##teamcity[testStarted name='" + "testAddition");
 		Assert.assertEquals(13, calculator.addition(10, 3));
+		System.out.println("##teamcity[testFinished name='" + "testAddition");
 	}
 	
 	public void testSubstraction() {
+		System.out.println("##teamcity[testStarted name='" + "testSubstraction");
 		Assert.assertEquals(2, calculator.substraction(5, 3));
+		System.out.println("##teamcity[testFinished name='" + "testAddition");
 	}
 	
 	public void testMultiplication() {
+		System.out.println("##teamcity[testStarted name='" + "testMultiplication");
 		Assert.assertEquals(15, calculator.multiplication(5, 3));
+		System.out.println("##teamcity[testFinished name='" + "testAddition");
 	}
 	
 	public static void main(String args[]) {
